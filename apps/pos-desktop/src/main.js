d8aa21cc111bd51c7fce34b3313f3db9a44a3b2e
Youssef Hage <youssefhage89@gmail.com>
2026-02-10 11:55:37 +0200

    Cloud: use api.melqard.com for API base

--- apps/pos-desktop/src/main.js
+++ apps/pos-desktop/src/main.js
@@ -27,7 +27,7 @@ function setStatus(msg) {
 }
 
 function load() {
-  el("edgeUrl").value = localStorage.getItem(KEY_EDGE) || "https://app.melqard.com/backend";
+  el("edgeUrl").value = localStorage.getItem(KEY_EDGE) || "https://api.melqard.com";
   el("portOfficial").value = localStorage.getItem(KEY_PORT_OFFICIAL) || "7070";
   el("portUnofficial").value = localStorage.getItem(KEY_PORT_UNOFFICIAL) || "7072";
   el("companyOfficial").value = localStorage.getItem(KEY_CO_OFFICIAL) || "00000000-0000-0000-0000-000000000001";
