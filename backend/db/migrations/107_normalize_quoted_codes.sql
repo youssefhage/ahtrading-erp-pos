3df1626e53b849eb88fb0126de40dc663e4c68b4
Youssef Hage <youssefhage89@gmail.com>
2026-02-10 08:27:20 +0200

    Fix migration 107: avoid unique conflicts when stripping quotes

--- backend/db/migrations/107_normalize_quoted_codes.sql
+++ backend/db/migrations/107_normalize_quoted_codes.sql
@@ -15,15 +15,32 @@ BEGIN;
 
 UPDATE items
 SET sku = regexp_replace(sku, '^\"+|\"+$', '', 'g')
-WHERE sku ~ '^\".+\"$';
+WHERE sku ~ '^\".+\"$'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM items i2
+    WHERE i2.company_id = items.company_id
+      AND i2.sku = regexp_replace(items.sku, '^\"+|\"+$', '', 'g')
+  );
 
 UPDATE customers
 SET code = regexp_replace(code, '^\"+|\"+$', '', 'g')
-WHERE code ~ '^\".+\"$';
+WHERE code ~ '^\".+\"$'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM customers c2
+    WHERE c2.company_id = customers.company_id
+      AND c2.code = regexp_replace(customers.code, '^\"+|\"+$', '', 'g')
+  );
 
 UPDATE suppliers
 SET code = regexp_replace(code, '^\"+|\"+$', '', 'g')
-WHERE code ~ '^\".+\"$';
+WHERE code ~ '^\".+\"$'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM suppliers s2
+    WHERE s2.company_id = suppliers.company_id
+      AND s2.code = regexp_replace(suppliers.code, '^\"+|\"+$', '', 'g')
+  );
 
 COMMIT;
-
