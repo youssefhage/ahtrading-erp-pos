65cbcff9420357a063f0e8a99b101f10b07e51aa
Youssef Hage <youssefhage89@gmail.com>
2026-02-13 03:47:29 +0200

    Clean old UI and fix scroll

--- pos-desktop/ui/vite.config.js
+++ pos-desktop/ui/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({
+    compilerOptions: {
+      runes: true,
+    },
+  })],
   build: {
     outDir: 'dist',
     emptyOutDir: true
